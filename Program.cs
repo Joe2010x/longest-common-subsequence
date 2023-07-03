daac8f2f828f17e44a57cf5ef8f6b6d5a49a6f44
Joe Sun <dps_swanuk@hotmail.com>
2023-07-03 22:56:08 +0200

    alternative approach

--- Program.cs
+++ Program.cs
@@ -1,48 +1,42 @@
 ﻿
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace LongestSubsequence
 {
     internal class Program
     {
-        public static string FindShortestString (string[] inputStrArr) {
-            var shortestStr = inputStrArr[0]; 
-            foreach (var item in inputStrArr)
-                if (shortestStr.Length > item.Length) shortestStr = item;
-            return shortestStr;
-        }
-        public static (string subString, bool isSubstring) IsOneSubstring(string shortestStr, int i, int j, string[] inputStrArr) {
-            var subString = shortestStr.Substring(i,j-i);
-                    var isSubstring = true;
-                    foreach (var item in inputStrArr) 
-                    {
-                        if (!(item.Contains(subString))) {
-                            isSubstring = false;
-                            break;
-                            }
-                    }
-            return (subString, isSubstring);
-        }
         public static string LongestSubsequence (string[] inputStrArr) {
-            var shortestStr = FindShortestString(inputStrArr);
             var result = "";
-            for (var i = 0; i<shortestStr.Length - 1; i++) {
-                for (var j = i + 1; j<shortestStr.Length; j++) {
-                    var (subString, isSubstring) = IsOneSubstring(shortestStr, i, j, inputStrArr);
-                    if (isSubstring & result.Length < subString.Length) result = subString;
+            var firstStr = inputStrArr[0];
+            var restStrArr = inputStrArr.Skip(1).ToArray();
+            foreach (var charX in firstStr)
+            {
+                var allContainCharX = true;
+                for (var i = 0; i<restStrArr.Length; i++) {
+                    if (!restStrArr[i].Contains(charX)) {
+                        allContainCharX = false;
+                        break;
+                    } 
+                }
+                if (allContainCharX) {
+                    for (var i= 0; i<restStrArr.Length; i++) {
+                        restStrArr[i] = restStrArr[i].Substring(restStrArr[i].IndexOf(charX)+1);
+                    }
+                    result += charX;
                 }
             }
             return result;
-            
         }
         static void Main(string[] args)
         {
-
             Console.WriteLine(
                 LongestSubsequence(new string[3] {
                     "epidemiologist", 
                     "refrigeration", 
-                    "supercalifragilisticexpialodocious"}));
+                    "supercalifragilisticexpialodocious"
+                    }));
         }
     }
 }
